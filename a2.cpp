d36198830cd98e5b8a5c486135a0846010ce9a76
Alex <soames.alex@gmail.com>
2021-02-19 17:13:14 -0800

    Initial Files

---
+++ a2.cpp
@@ -0,0 +1,47 @@
+// a2.cpp
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Student Info
+// ------------
+//
+// Name : Alex Soames
+// St.# : 301 292 784
+// Email: asoames@sfu.ca
+//
+//
+// Statement of Originality
+// ------------------------
+//
+// All the code and comments below are my own original work. For any non-
+// original work, I have provided citations in the comments with enough detail
+// so that someone can see the exact source and extent of the borrowed work.
+//
+// In addition, I have not shared this work with anyone else, and I have not
+// seen solutions from other students, tutors, websites, books, etc.
+//
+/////////////////////////////////////////////////////////////////////////
+
+//
+// These are the only permitted includes: don't include anything else.
+//
+#include "cmpt_error.h"
+#include <iostream>
+#include <string>
+#include <algorithm>       
+#include <cassert>
+
+using namespace std;
+
+class str_vec {
+
+    // ...
+
+}; // class str_vec
+
+
+int main() {
+  cout << "~~~Assignment 02~~~\n";
+
+
+}
